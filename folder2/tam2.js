6e0e3dd94e368d4feb421a30f40b79bbcb0199b7
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-16 07:47:58 -0800

    shekoofeh-shojai-classA

---
+++ folder2/tam2.js
@@ -0,0 +1,24 @@
+let str1='hello';
+let str2="hi gys i'm";
+///not use
+str1[0]='H'
+console.log(str1.charAt(0))
+//asci code
+console.log(str1.charCodeAt(0));
+console.log(str1.endsWith('mahya'));
+console.log(str2.repeat(2));
+console.log(str1.split(','));
+///opration
+//math
+//+ - ++ -- / %
+//asisgnment
+//= += -= /=
+
+//comparison
+//==  === != !== <>= <=
+//logical
+//&& || |
+
+let scope=10;
+scope+=5;
+console.log(scope++);
