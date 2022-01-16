d7f23c0cab327e6f0c02314f84de46b4547fbdee
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-16 06:51:37 -0800

    sekoofeh-shojai-classA

---
+++ folder1/tamrin.js
@@ -0,0 +1,52 @@
+let num = {
+  name: "dog",
+  legs: 4,
+  color: "white",
+};
+function animal(obj) {
+  console.log(obj.name);
+
+  //
+}
+animal(num);
+let poztive;
+poztive = 12;
+poztive++;
+//quiz
+let water = 100;
+water /= 2;
+water += 10;
+console.log();
+//error
+const myage = 29;
+myage++;
+///////
+Number("123") === 123;
+//////boolean
+let isloogin = true;
+////////string
+let animal = "dog";
+animal = "dog";
+animal = `cat`;
+//
+// let space="ali\arad\"
+///concatination
+let str1 = "dd";
+let str2 = "cc";
+console.log(str1 + str2);
+////quiz
+let year = "2000";
+console.log(year + 1);
+////////string metood
+let upper = str1.toUpperCase();
+let lower = str2.trim();
+str2.charAt(1);
+///
+indexof();
+repleaseAll();
+repeat();
+Math.random();
+Math.PI;
+///gerd b samt bala
+Math.ceil();
+// b kaf round mikonad
