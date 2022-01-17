544380c92d0da5c90b1fcaa4a932a1bfd23beeda
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-17 00:01:26 -0800

    shekoofeh-shojai-classA

--- folder2/tam2.js
+++ folder2/tam2.js
@@ -1,13 +1,15 @@
 let str1='hello';
 let str2="hi gys i'm";
 ///not use
-str1[0]='H'
-console.log(str1.charAt(0))
+// str1[0]='H'
+// console.log(str1.charAt(0))
+
 //asci code
-console.log(str1.charCodeAt(0));
-console.log(str1.endsWith('mahya'));
-console.log(str2.repeat(2));
-console.log(str1.split(','));
+// console.log(str1.charCodeAt(0));
+// console.log(str1.endsWith('mahya'));
+// console.log(str2.repeat(2));
+// console.log(str1.split(','));
+
 ///opration
 //math
 //+ - ++ -- / %
@@ -19,36 +21,56 @@ console.log(str1.split(','));
 //logical
 //&& || |
 
-let scope=10;
-scope+=5;
-console.log(scope++);
+// let scope=10;
+// scope+=5;
+// console.log(scope++);
+
 //////////////////////////////
 //200+0/0=nun
 //ca'nt taghir
-const myAge=29
-myAge++;
-console.log(myAge);
+// const myAge=29
+// myAge++;
+// console.log(myAge);
+
 ////////////type conversion
 //manual
-const num=prompt('inter number')
-console.log(Number(num)+10)
-console.log("5"+null)
+
+// const num=prompt('inter number')
+// console.log(Number(num)+10)
+// console.log("5"+null)
+
+
 //function
 //declartion
 //expration
 //arrow
-function sum(num1,num2){
-    console.log(num1+num2)
-}
 
-sum(6, 9);
+
+// function sum(num1,num2){
+//     console.log(num1+num2)
+// }
+
+// sum(6, 9);
 //arrow
 let a=()=>{
 
 }
 
-const arr=['a','b','c','g'];
+// const arr=['a','b','c','g'];
 
-for (let i = 0; i < arr.length; i++) {
-  const element = array[i];
-}
+// for (let i = 0; i < arr.length; i++) {
+//   const element = array[i];
+// }
+const numbers = [2, 4, 6,7,8,9];
+// numbers.push(12)
+// console.log(numbers);
+// numbers.splice(1, 0, "Feb").slice(2,4);
+// console.log(numbers)
+
+let n=numbers.slice(2);
+console.log(n)
+
+
+const animals = [1, 2, 3, 4, 5];
+
+console.log(animals.slice(3,5));
