82c020726b78a1a95d99b2e9ae846615de817197
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-17 12:51:13 -0800

    shekoofeh-shojai-classa

--- folder2/tam2.js
+++ folder2/tam2.js
@@ -61,16 +61,3 @@ let a=()=>{
 // for (let i = 0; i < arr.length; i++) {
 //   const element = array[i];
 // }
-const numbers = [2, 4, 6,7,8,9];
-// numbers.push(12)
-// console.log(numbers);
-// numbers.splice(1, 0, "Feb").slice(2,4);
-// console.log(numbers)
-
-let n=numbers.slice(2);
-console.log(n)
-
-
-const animals = [1, 2, 3, 4, 5];
-
-console.log(animals.slice(3,5));
