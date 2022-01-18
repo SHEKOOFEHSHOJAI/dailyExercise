5ea37a5565afbb986c7f65978b95f7147a31eb01
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-18 07:46:55 -0800

    shekoofeh-shoja-classa

--- folder4/tamrin.js
+++ folder4/tamrin.js
@@ -1,24 +1,24 @@
-const asgar = {
-  ram: 8,
-  hard: 128,
-  cpu: "core2",
-};
-const coputer=({ram,hard,cpu})=>`this is ram ${ram} this is hard${hard}and ${cpu}`;
- console.log(coputer(asgar));
+// const asgar = {
+//   ram: 8,
+//   hard: 128,
+//   cpu: "core2",
+// };
+// const coputer=({ram,hard,cpu})=>`this is ram ${ram} this is hard${hard}and ${cpu}`;
+//  console.log(coputer(asgar));
 
 
-const scoreOfRallyRace = [3500, 3799, 4000, 8000, 8500, 9000, 12000];
- const [winner, ...others] = scoreOfRallyRace;
-let r=scoreOfRallyRace[0]=6;
-console.log(scoreOfRallyRace);
+// const scoreOfRallyRace = [3500, 3799, 4000, 8000, 8500, 9000, 12000];
+//  const [winner, ...others] = scoreOfRallyRace;
+// let r=scoreOfRallyRace[0]=6;
+// console.log(scoreOfRallyRace);
 
 
 
-const  passenger = {
-  drunk: false,
-  age: 20,
-  isDrivingLicence: true,
-};
+// const  passenger = {
+//   drunk: false,
+//   age: 20,
+//   isDrivingLicence: true,
+// };
 
 // const { cylinder, passengers, ...others } = passenger;
 
@@ -28,14 +28,58 @@ const  passenger = {
 
 
 
-const[first,...me]=["ali",4,6,90,"60"];
-const item=[...me]
-console.log(item)
-const item1 = { ...me };
-console.log(item1);
+// const[first,...me]=["ali",4,6,90,"60"];
+// const item=[...me]
+// console.log(item)
+// const item1 = { ...me };
+// console.log(item1);
 
-const obj = {
-  ali: "dat",
-  null: "jafar",
+// const obj = {
+//   ali: "dat",
+//   null: "jafar",
+// };
+// console.log(obj['ali']);
+
+
+
+const person = {
+  firstName: "naser",
+  lastName: "Gyllenhaal",
+  age: 40,
+  born: "italy",
 };
-console.log(obj[ali]);
+
+// console.log(person.firstName);
+// console.log(person["firstName"]);
+// console.log(person["first" + "Name"]);
+
+
+
+let firstName = "age";
+// console.log(person[firstName]);
+
+
+
+
+
+
+let totalEggs = 0;
+function collectEggs() {
+  totalEggs = 6;
+}
+
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+function call(){
+roll()
+roll()
+roll();
+}
+function roll(){
+    const rol=(Math.random()*5)+1
+    console.log(rol)
+
+}
+call(roll)
