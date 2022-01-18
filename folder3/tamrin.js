41a52879df419ffa9cd826058c3e9f2bdcdefd26
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-18 00:30:47 -0800

    shekoofeh-shojai-classa

--- folder3/tamrin.js
+++ folder3/tamrin.js
@@ -11,39 +11,39 @@ const animals = [1, 2, 3, 4, 5];
 
 console.log(animals.slice(3, 5));
 
-let laptop = {
-  brand: "Lenovo",
-  screenSize: 13,
-  isTouchscreen: true,
-  colors: ["spaceGray", "black", "white"],
-  spec: {
-    owner: "vahid",
-    age: 24,
-    height: undefined,
-  },
-};
+// let laptop = {
+//   brand: "Lenovo",
+//   screenSize: 13,
+//   isTouchscreen: true,
+//   colors: ["spaceGray", "black", "white"],
+//   spec: {
+//     owner: "vahid",
+//     age: 24,
+//     height: undefined,
+//   },
+// };
 
-console.log(laptop.brand)
-console.log(laptop["spec"]["owner"]);
-console.log(laptop["spec"]["age"]);
-console.log(laptop.colors[1][3]);
+// console.log(laptop.brand)
+// console.log(laptop["spec"]["owner"]);
+// console.log(laptop["spec"]["age"]);
+// console.log(laptop.colors[1][3]);
 
-const mystry="answer"
-let inverseOfPI=12;
-const ali={
+// const mystry="answer"
+// let inverseOfPI=12;
+// const ali={
    
-     d1:()=>{},//arrow
-     [mystry] : 21,
-    inverseOfPI
-}
+//      d1:()=>{},//arrow
+//      [mystry] : 21,
+//     inverseOfPI
+// }
 
-console.log(ali.inverseOfPI);
-console.log(ali[mystry]); 
- console.log(ali.answer)
- ;
+// console.log(ali.inverseOfPI);
+// console.log(ali[mystry]); 
+//  console.log(ali.answer)
+//  ;
 
      
- console.log(ali[mystry]);
+//  console.log(ali[mystry]);
  
 
 
@@ -66,6 +66,23 @@ console.log(ali[mystry]);
  console.log("this is jasem2",this)
    }
  }
- console.log(test.jasem())
- console.log(test.jasem1())
- console.log(test.jasem2())
+//  console.log(test.jasem())
+//  console.log(test.jasem1())
+//  console.log(test.jasem2())
+
+
+//////destructuring
+// const rol=(max=6)=>{
+// let rand=Math.floor(Math.random()*max)+1
+// return console.log(rand)
+// }
+
+// rol()
+//varable destucture
+const{IP ,random, e,max}=Math;
+
+
+console.log(max(1,2,3));
+//Array destuctureing
+const[first,sec,thid]=[12,13,14];
+console.log(first)
