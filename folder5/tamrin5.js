eb2864e57d95582cccf5b444caca7c4ab740e322
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-21 00:28:24 -0800

    shekoofeh-shojai-class-a

--- folder5/tamrin5.js
+++ folder5/tamrin5.js
@@ -1,34 +1,52 @@
-let =[]
-const yellow=[" Reply hazy, try again.","Ask again later."
-,"Better not tell you now.","Cannot predict now.","Concentrate and ask agai"];
-
-
-
- const red=[" Don't count on it.","My reply is no.","My sources say no."
- ,"Outlook not so good.","Very doubtful."]
 
 
 
 
-const color = [ red, yellow];
+// const color = ["red" , "yellow","green"];
    
     
 //    let map= color.map((elem)=>{
 //       let b = color[ Math.floor(Math.random() * color.length) + 1];   
 //  console.log(b)
 //     })
-    function nam(params) {
-          let b = params[Math.floor(Math.random() * color.length-1) + 1];
-          console.log(b);
-          if(b==="red"){
-              console.log("negtive"+color[0])
-          }
-          else if(b==="yellow"){
-             console.log("nachral")
-          }
-          else if(b==="green"){
-              console.log("posetive")
-          }
+    // function nam(params) {
+    //       let b = params[Math.floor(Math.random() * color.length-1) + 1];
+    //       console.log(b);
+    //       if(b==="red"){
+    //           console.log("negtive"+color[0])
+    //       }
+    //       else if(b==="yellow"){
+    //          console.log("nachral")
+    //       }
+    //       else if(b==="green"){
+    //           console.log("posetive")
+    //       }
      
-    }
-    nam(color)
+    // }
+    // nam(color)
+
+
+
+
+  
+    const color = {
+      yellow: [
+        " Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask agai",
+      ],
+
+      red: [
+        " Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful.",
+      ],
+    };
+
+
+   let b = color[Math.floor(Math.random() * color.length) + 1]; 
+   console.log(b);  
