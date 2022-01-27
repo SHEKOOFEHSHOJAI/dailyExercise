26418558aa4fe32786d526d6aa6dc13e16539800
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-01-27 00:48:19 -0800

    shekoofeh-shojai-class-a

--- folder8/tmarin.js
+++ folder8/tmarin.js
@@ -1,6 +1,6 @@
 ////////quiz made rainbow
-const color=["red","violt","green","blue","yellow"];
-let n=document.querySelectorAll('h1 span')
+const color = ["red", "violt", "green", "blue", "yellow"];
+let n = document.querySelectorAll("h1 span");
 // let count=0;
 // for (const iterator of n) {
 //     console.log(n)
@@ -8,15 +8,12 @@ let n=document.querySelectorAll('h1 span')
 //     count++;
 // }
 
-
 ////foreach
 // n.forEach((ele,index)=>console.log(ele.style.color= color[index]))
-
 
 ////map  becuse map give array use the array.form
 // let mp=Array.from(n).map((ele,index)=>console.log(ele.style.color=color[index]))
 // console.log(mp);
-
 
 ////////
 // let mp = Array.from(n).map(({style}, index) =>
@@ -24,21 +21,20 @@ let n=document.querySelectorAll('h1 span')
 // );
 // console.log(mp);
 
-
 //////////////made 100button
-for (let i = 1; i <=100; i++) {
-    const button = document.createElement("button")
-    button.textContent=`i am button ${i}`
-    button.style.backgroundColor="red";
-    button.style.borderRadius="8px"
-    button.style.borderLeft="10px"
-    button.style.margin="4px"
-   
-  document.body.appendChild(button) 
- 
-    button.onclick=(()=> button.style.backgroundColor="blue")
+for (let i = 1; i <= 100; i++) {
+  const button = document.createElement("button");
+  button.textContent = `i am button ${i}`;
+  button.style.backgroundColor = "red";
+  button.style.borderRadius = "8px";
+  button.style.borderLeft = "10px";
+  button.style.margin = "4px";
+
+  document.body.appendChild(button);
+
+  button.onclick = () => (button.style.backgroundColor = "blue");
 }
-//  
+//
 
 //////////remove element
 // const a = document.querySelector("a");
@@ -46,7 +42,6 @@ for (let i = 1; i <=100; i++) {
 // prent.removeChild(a);
 // console.log(prent);
 
-
 ////addeventlistener=(event,function)
 
-button.addEventListener("dbclick",()=>console.log("click me"))
+button.addEventListener("dbclick", () => console.log("click me"));
