0bb41baa9aa995b50b32b340db8b5e998f4ef615
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-04 05:33:43 -0800

    shekoofeh-shojai-class-A

---
+++ folder12/tamrin.js
@@ -0,0 +1,10 @@
+const v2=document.getElementById("v2")
+function scr(params) {
+    console.log("hover");
+}
+// v2.onmouseleave=scr
+
+//  v2.onmousedown = scr;
+//  v2.onmouseover = scr;
+//   v2.onmouseenter = scr;
+//  v2.onmousemove = scr;
