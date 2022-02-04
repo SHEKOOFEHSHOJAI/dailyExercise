4aa97fffa0ea608465cd3a2bc343c8ebc77f4a79
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-04 07:33:56 -0800

    shekoofeh-shojai-class-A

--- folder11/tamrin.js
+++ folder11/tamrin.js
@@ -10,3 +10,39 @@ let mouseMove=()=>{
   let message = document.getElementById("message").textContent="moveee";  
 }
 face.addEventListener("mouseMove", mouseMove);
+
+///we want click on button show hello shekoofeh
+let button=document.getElementById("button")
+let onButtonClick=function (params) {
+    let name=document.getElementById("name").Value
+    let message = document.getElementById("message").textContent="greet";
+    let greet;
+    let lang = document.getElementById("lang").Value;
+    if(lang==="es"){
+    greet="hola"+name;
+    }
+    else if (lang === "plt") {
+      greet = "hola" + name;
+    }
+  
+}
+button.addEventListener("click", onButtonClick);
+
+//preventDefault()    از رفتار پیشفرض جلوگیری میکند 
+
+
+
+
+///Removing event listeners
+
+var faceEl = document.getElementById("face");
+var onFaceClick = function(e) {
+    console.log("You clicked " + e.clientX + " , " + e.clientY);
+};
+faceEl.addEventListener("click", onFaceClick);
+// later...
+faceEl.removeEventListener("click", onFaceClick);
+
+
+
+
--- folder12/tamrin.js
+++ folder12/tamrin.js
@@ -8,3 +8,11 @@ function scr(params) {
 //  v2.onmouseover = scr;
 //   v2.onmouseenter = scr;
 //  v2.onmousemove = scr;
+
+// const height = document.getElementById("height");
+// const width = document.getElementById("width");
+// function name(params) {
+//     console.log(window);
+//     width.textContent=window.innerWidth
+// }
+// window.addEventListener("resize",name)
