de17b92b4e33592b3b8ba7db6b534d9cab072a1e
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-05 01:13:21 -0800

    shekoofeh-shojai-class-A

--- folder13/tamrin.js
+++ folder13/tamrin.js
@@ -1,9 +1,37 @@
 /////bazi
 const input = document.querySelector("input");
+console.log(input);
 let baziBtn = document.querySelector("#evenBtn");
-
+console.log(baziBtn);
 input.addEventListener("keydown", (e) => {
   console.log(e.code);
-  //   switch (e.code) {
-  //   }
+    switch (e.code) {
+      case "ArrowUp":
+        console.log("up");
+        break;
+      case "ArrowDown":
+        console.log("down");
+        break;
+
+      case "ArrowLeft":
+        console.log("left");
+        break;
+      case "ArrowRight":
+        console.log("right");
+        break;
+      default:
+        console.log(";)");
+    }
 });
+
+
+const div=document.querySelector(".test")
+div.classList.add("circle");
+document.body.addEventListener("keydown",(e)=>{
+   
+    console.log(e);
+
+})
+
+
+
