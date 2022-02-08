acb7cdbd9336ba3320eb81501d28a88f7f94e674
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-08 05:47:20 -0800

    shekoofeh-shojai-classA

--- folder14/tamrin.js
+++ folder14/tamrin.js
@@ -1,9 +1,60 @@
-const delay=(newColor,dealy) =>{
-setTimeout(()=>{
-document.body.style.backgroundColor = newColor;
-},dealy)
-};
-delay("red",2000)
-delay("green", 3000);
-delay("yellow", 4000);
+// const delay=(newColor,dealy) =>{
+// setTimeout(()=>{
+// document.body.style.backgroundColor = newColor;
+// },dealy)
+// };
+// delay("red",2000)
+// delay("green", 3000);
+// delay("yellow", 4000);
 
+// const fakerequst=(url,sucses,fail)=>{
+//     const daily =Math.floor(Math.random()*4500)+500;
+//     setTimeout(()=>{
+//         if (daily>4000){
+//             fail("coloction time out")
+//         }
+//         else{
+//             sucses("here is your fak")
+//         }
+//     })
+//     console.log(daily);
+// }
+//  fakerequst("promis.com")
+//  .then(data=>{
+//      console.log("page1=>");
+//  })
+
+
+
+
+
+let laptop = {
+  brand: "Lenovo",
+  screenSize: 13,
+  isTouchscreen: true,
+  colors: ["spaceGray", "black", "white"],
+  spec:{
+    owner: 'vahid' , 
+    age: 24,
+    height: undefined
+  }
+}
+
+console.log(laptop["brand"])
+console.log(laptop.spec.owner);
+console.log(laptop["spec"]["age"]);
+console.log(laptop["colors"][1]);
+console.log();
+
+
+
+
+
+
+const ali={
+    nameFamiy:"naderi",
+    age:12,
+    from:"iran"
+}
+const {nameFamiy,age,from}=ali
+console.log();
