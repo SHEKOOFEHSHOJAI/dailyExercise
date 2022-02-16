98a80aafa46c3140908ac57d022638c37fc4ef1e
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-16 01:35:48 -0800

    shekoofeh-shojai-classA

--- folder16/tamrin.js
+++ folder16/tamrin.js
@@ -1,27 +1,73 @@
- document.querySelector("#button").addEventListener("click",loadData)
-    //made shy
+ 
+ //Ajxs
+ 
+//  document.querySelector("#button").addEventListener("click",loadData)
+//     //made shy
    
 
-    function loadData() {
+//     function loadData() {
       
-         const xhr = new XMLHttpRequest();
-         //open rqust
+//          const xhr = new XMLHttpRequest();
+//          //open rqust
 
-         xhr.open("GET", "data.text", true);
+//          xhr.open("GET", "data.text", true);
 
-         ///load
+//          ///load
 
-        xhr.onload = function () {  
-           if (this.status === 200) {
-             document.querySelector(
-               "#output"
-             ).innerHTML = `<h1>${this.responseText}</h1>`;
-             console.log("ok");
-           }
+//         xhr.onload = function () {  
+//            if (this.status === 200) {
+//              document.querySelector(
+//                "#output"
+//              ).innerHTML = `<h1>${this.responseText}</h1>`;
+//              console.log("ok");
+//            }
+//          }
+//          xhr.send();
+
+
+
+//     }
+
+
+    ////////////////////////////////////
+
+    ///////////////////////////////////
+
+   function piza() {
+     return new Promise((risolve,reject)=>{
+         if(true){
+           risolve("piza is ready")
          }
-         xhr.send();
-
-
-
+         else{
+           reject("sorry we don't have piza")
+         }
+     })
     }
-   
+
+    function soda() {
+    return new Promise((risolve,reject)=>{
+
+      if (false) {
+        risolve("soda is ready");
+      }
+      
+      else {
+        reject("sorry we don't have soda");
+      }
+
+
+    })
+
+      
+    }
+    piza().then(msg=>{
+      console.log(msg);
+      return soda()
+    }).then(msg=>{
+      console.log(msg);
+    }).catch(error=>{
+      console.log(error);
+    })
+
+
+    
---
+++ folder17/tamrin.js
@@ -0,0 +1,22 @@
+document.querySelector("button").addEventListener("click", local);
+
+function local() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "data.text", true);
+
+  xhr.onload = function name(params) {
+    if (this.status === 200) {
+      const h1 = (document.createElement("h1").textContent = this.responseText);
+      const div = document.querySelector("output");
+      div.append(h1);
+    }
+  };
+  xhr.send();
+}
+
+//status
+//status 200 => corrct
+//404=>not found
+//403=>forbident  =>tahrim
+//readyState
+//
