01d8a2bbd80022727dd95bf93ca7ba1774824441
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-23 23:01:56 -0800

    shekoofeh-shojai-classA

--- folder19/tamrin.js
+++ folder19/tamrin.js
@@ -1,1 +1,33 @@
 //Iterate Over All Properties
+
+function Bird(name) {
+  this.name = name; //own property
+}
+
+Bird.prototype.numLegs = 2; // prototype property
+
+let duck = new Bird("Donald");
+
+
+
+//Add all of the own properties of beagle to the array ownProps.
+// Add all of the prototype properties of Dog to the array prototypeProps.
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+for (let be in beagle) {
+  if (beagle.hasOwnProperty(be)) {
+    ownProps.push(be);
+  } else {
+    prototypeProps.push(be);
+  }
+}
