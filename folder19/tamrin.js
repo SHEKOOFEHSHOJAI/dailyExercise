b86a0aaa999d07722c1b9805b182b623620cdddc
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-24 05:17:34 -0800

    shekoofe-shojai-classA

--- folder19/tamrin.js
+++ folder19/tamrin.js
@@ -136,3 +136,28 @@ Animal.prototype = {
 
 let duck=Object.create(Animal.prototype); // Change this line
 let beagle=Object.create(Animal.prototype); // Change this line
+
+
+
+
+//But duck and all instances of Bird should show that they were constructed by Bird and not Animal. To do so, 
+//you can manually set the constructor property of Bird to the Bird object:
+//  qustion:=>Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+
+
+function Animal() {}
+function Bird() {}
+function Dog() {}
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Bird.prototype);
+
+// Only change code below this line
+
+let duck = new Bird();
+let beagle = new Dog();
+Bird.prototype.constructor = Bird;
+duck.constructor;
+
+Dog.prototype.constructor = Dog;
+beagle.constructor;
