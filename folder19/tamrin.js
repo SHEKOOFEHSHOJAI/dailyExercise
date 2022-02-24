46d19c19449b0466f5fde58cba30d5b9a64243c7
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-23 23:28:01 -0800

    shekoofeh-shojai-classA

--- folder19/tamrin.js
+++ folder19/tamrin.js
@@ -31,3 +31,22 @@ for (let be in beagle) {
     prototypeProps.push(be);
   }
 }
+
+
+
+
+
+//Write a joinDogFraternity function that takes a candidate parameter and, using the constructor property,
+// return true if the candidate is a Dog, otherwise return false.
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor == Dog) {
+    return true;
+  } else {
+    return false;
+  }
+}
