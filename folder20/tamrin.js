6d16bd94a13fd6d9b226f74039d76f6f1f10ea30
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-02-26 05:47:42 -0800

    shekoofeh-shojai-classA

--- folder20/tamrin.js
+++ folder20/tamrin.js
@@ -105,3 +105,21 @@ const { today, tomorrow } = HIGH_TEMPERATURES;
 
 
 
+
+
+///////Use Destructuring 
+//Assignment to Assign Variables from Objects
+
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80,
+};
+
+
+const{ today: highToday, tomorrow: highTomorrow }
+=HIGH_TEMPERATURES;
+
+
+
