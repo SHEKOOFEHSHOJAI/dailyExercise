a99979ce9b8ef49a32a725ac1a119317cb362042
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-02 11:22:02 -0800

    shekoofeh-shojai-classA

--- folder22/tamrin.js
+++ folder22/tamrin.js
@@ -12,18 +12,28 @@ function Student() {
 // console.dir(Student);
 
 
-const myObject = {
-  city: 'Madrid',
-  greet() {
-    console.log(`Greetings from ${this.city}`);
-  }
-}
+// const myObject = {
+//   city: 'Madrid',
+//   greet() {
+//     console.log(`Greetings from ${this.city}`);
+//   }
+// }
 
 // myObject.greet(); // Greetings from Madrid
 // console.log(myObject);
 // let b=myObject.toString();=> result=>[object Object]
 
 // returns the prototype
-let proto = Object.getPrototypeOf(myObject);
-console.log(proto);
+// let proto = Object.getPrototypeOf(myObject);
+// console.log(proto);
 // var obj = Object.create(proto);
+
+
+
+
+// const arr = [1, 2, 3, 4];
+// arr.met=function name(params) {
+//   console.log(hello);
+// }
+// arr.push(5);
+// console.log(arr);
