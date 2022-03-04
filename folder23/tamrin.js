65ee84ae5b36ed304cb99c8c738570e95b397ce4
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-03 22:21:11 -0800

    shekoofeh-shojai-classA

---
+++ folder23/tamrin.js
@@ -0,0 +1,12 @@
+const st=String.prototype.po=function() {
+    
+}
+console.dir(st);
+function nam(params) {
+    
+}
+console.log(nam);
+let u=String.prototype.yell = function () {
+  console.log(this.toUpperCase());
+};
+console.log(u);
