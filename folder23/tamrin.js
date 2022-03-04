af4a3e90385908cac2dec477804e73aab104ad86
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-03 23:36:41 -0800

    shekoofeh-shojai-classA

--- folder23/tamrin.js
+++ folder23/tamrin.js
@@ -1,12 +1,34 @@
-const st=String.prototype.po=function() {
+// const st=String.prototype.po=function() {
     
-}
-console.dir(st);
-function nam(params) {
+// }
+// console.dir(st);
+// function nam(params) {
     
-}
-console.log(nam);
-let u=String.prototype.yell = function () {
-  console.log(this.toUpperCase());
-};
-console.log(u);
+// }
+// console.log(nam);
+// let u=String.prototype.yell = function () {
+//   console.log(this.toUpperCase());
+// };
+// console.log(u);
+
+// const arr = [1, 2, 3, 4];
+// arr.sing = function () {
+//   return console.log("hala lalay lalay hala lay lay");
+// };
+// console.log(arr);
+
+// function make(username,email) {
+//     const user={};
+//     user.nam=username;
+//     user.emailAdress=email;
+//     user.login=function(){
+//         const { nam, emailAdress } = this;
+//         alert(`user named ${nam} has this email:
+//           ${emailAdress}`); ;
+//     }
+//     return user
+// }
+
+// // console.log(user);
+// const jasem = make("jasem", "jasem@gmail.com");
+// console.log(jasem);
