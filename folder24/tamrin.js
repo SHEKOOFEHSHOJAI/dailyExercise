b80329629027756449217b26755eb178dbde3468
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-06 00:25:28 -0800

    shekoofeh-shojai-classA

--- folder24/tamrin.js
+++ folder24/tamrin.js
@@ -1,7 +1,27 @@
-let rectangle=class SHEKOO{
-    constructor(height,width){ 2
-     this.height=height
-     this.width=width
-    }
+// let rectangle=class SHEKOO{
+//     constructor(height,width){ 2
+//      this.height=height
+//      this.width=width
+//     }
+// }
+// console.log(rectangle.name);
+
+
+
+
+//use strict
+//https://pvlearn.com/
+// "use strict";
+// var arguments = 3.14; 
+
+
+// چرا باید درون کلاس از کانسترکتور استفاده کنیم
+////class
+class colur{
+ constructor(r,g,b){
+    this.red=r
+    this.green=g
+    this.blue=b
+ }
 }
-console.log(rectangle.name);
+console.log(colur);
