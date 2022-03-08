0537e9ce087b97cd435ab24d1ac61ffd1623b1cc
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-07 22:24:48 -0800

    shekoofeh-shojai-classA

--- folder25/tamrin.js
+++ folder25/tamrin.js
@@ -1,17 +1,37 @@
-class user{
-    constructor(usename,email){
-        this.usern=usename
-        this.email=email
+// class user{
+//     constructor(usename,email){
+//         this.usern=usename
+//         this.email=email
+//         console.log("This is a car");
 
-    }
-}
+//     }
+// }
 
-class sirvan extends user{
-    constructor(usename,email,title){
-        super(usename, email);
-        this.title=title
-    }
+// class sirvan extends user{
+//     constructor(){
+//         // super(usename, email);
+//         // this.title=title
+//         console.log("this is tas");
+//     }
     
+// }
+// const person1 = new sirvan("kazem", "kazem@email.com");
+// console.log(person1);
+//https://www.aparat.com/v/JPxr2/%D8%A2%D9%85%D9%88%D8%B2%D8%B4_%D9%85%D8%AA%D8%AF_super_%D8%AF%D8%B1_%D8%AC%D8%A7%D9%88%D8%A7%D8%A7%D8%B3%DA%A9%D8%B1%DB%8C%D9%BE%D8%AA
+
+
+class Car {
+  constructor() {
+    console.log("This is a car");
+  }
 }
-const person1 = new user("kazem", "kazem@email.com");
-console.log(person1);
+
+class Tesla extends Car {
+  constructor() {
+    console.log("This is a Tesla");
+  }
+}
+
+
+const myCar = new Tesla();
+console.log(myACar);
