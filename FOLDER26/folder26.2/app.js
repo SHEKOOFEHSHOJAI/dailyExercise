0dbdb31e1ef358a6ff184dad323754692ad7b44f
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-08 12:31:09 -0800

    shekoofeh-shojai-classA

---
+++ FOLDER26/folder26.2/app.js
@@ -0,0 +1,3 @@
+const { PI, square } = require("./math");
+module.exports = { PI, square };
+console.log(module);
---
+++ FOLDER26/folder26.2/math.js
@@ -0,0 +1,4 @@
+const square = (x) => console.log(x * x);
+const PI = 3.14159;
+module.exports = { PI, square };
+console.log(module);
