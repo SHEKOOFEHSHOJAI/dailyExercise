98c7845aa7c159421c65f79bcaa289acd3ad677a
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-03-08 00:15:08 -0800

    shekoofeh-shojai-classA

--- folder25/tamrin.js
+++ folder25/tamrin.js
@@ -20,18 +20,84 @@
 //https://www.aparat.com/v/JPxr2/%D8%A2%D9%85%D9%88%D8%B2%D8%B4_%D9%85%D8%AA%D8%AF_super_%D8%AF%D8%B1_%D8%AC%D8%A7%D9%88%D8%A7%D8%A7%D8%B3%DA%A9%D8%B1%DB%8C%D9%BE%D8%AA
 
 
-class Car {
-  constructor() {
-    console.log("This is a car");
+// class Car {
+//   constructor() {
+//     console.log("This is a car");
+//   }
+// }
+
+// class Tesla extends Car {
+//   constructor() {
+//       super();
+//     console.log("This is a Tesla");
+//   }
+// }
+
+
+// const myCar = new Tesla();
+// console.log(myACar);
+
+////////////////////////////
+
+////////////////////////////
+// class Car {
+//   constructor() {
+//     console.log("This is a car");
+//   }
+// }
+// const myCar = new Car();
+
+///result
+////'This is a car'
+
+//این خودش ابجکته  چرا باید با کانستراکتور ی ابجکت جدید درونش ساخت
+// class pet{
+//     constructor(name,age){
+//        this.na=name
+//        this.age=age
+
+//     }
+//     eat(){
+//         console.log("heelo");
+//         return `${this.na} is eating`
+//     }
+// }
+//  class cat extends pet {
+//    constructor(name, livesleft = 22) {
+//      super(name);
+//      this.livesleft = livesleft;
+//    }
+//    meow() {
+//      return "MEOWWW!!!";
+//    }
+//  }
+// const eat = new cat("pishi", 3,9);
+// console.log(eat);
+
+
+
+/////////////////////چرا اینجاعدد 9 بهش داده نمیشه و مقدار اندیفاین است مقدار سن
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    
+  }
+
+  eat() {
+    return `${this.name} is eating!`;
   }
 }
 
-class Tesla extends Car {
-  constructor() {
-    console.log("This is a Tesla");
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age);
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return "MEOWWW!!!";
   }
 }
 
-
-const myCar = new Tesla();
-console.log(myACar);
+const gorbe = new Cat("pishi", 3);
+console.log(gorbe);
